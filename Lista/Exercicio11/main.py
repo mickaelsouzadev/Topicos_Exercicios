ef9b0597920efef641deed1e44c59167bd911bc2
Mickael Souza <mickael.souza.if@gmail.com>
2018-04-16 08:50:13 -0300

    Exericios da Lista

--- Lista/Exercicio11/main.py
+++ Lista/Exercicio11/main.py
@@ -1,31 +1,23 @@
-
-#def intercalar(vetor1, vetor2):
-
-    #vetorIntercalado = []
-
-    #for v1, v2 in zip(vetor1, vetor2):
-        #vetorIntercalado.append(v1)
-        #vetorIntercalado.append(v2)
-
-    #return vetorIntercalado
-
 def intercalar(lG):
     lI = []
-    for i in range(len(lG) + 1):
+    for i in range(len(lG)):
         for j in range(len(lG)):
             lI.append(lG[j][i])
     return lI
 
 vetor1 = []
 vetor2 = []
+vetor3 = []
 
 for i in range(10):
     vetor1.append(input("Digite um valor pro vetor 1: "))
     vetor2.append(input("Digite um valor por vetor 2: "))
+    vetor3.append(input("Digite um valor por vetor 3: "))
 
 lG = []
 lG.append(vetor1)
 lG.append(vetor2)
+lG.append(vetor3)
 
 vetorIntecalado = intercalar(lG)
 
